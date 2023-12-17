ccf42eea7313530f531fa5974f3552849f655eb4
VillDev <NachoViCu16@gmail.com>
2023-12-17 20:52:13 +0100

    Hide panels

--- js/ui.js
+++ js/ui.js
@@ -7,22 +7,35 @@ $(document).ready(function() {
 
     $('.logo').click(function() {
         console.log("Home")
-        $('#projects-link').removeClass("active")
         $('#about-me-link').removeClass("active")
+        $('#projects-link').removeClass("active")
+
+        $('#home-page').removeClass("hide")
+        $('#about-me-page').addClass("hide")
+        $('#projects-page').addClass("hide")
     })
 
     $('#about-me-link').click(function() {
         console.log("About me")
         $('#projects-link').removeClass("active")
         $(this).addClass("active")
+
+        $('#home-page').addClass("hide")
+        $('#about-me-page').removeClass("hide")
+        $('#projects-page').addClass("hide")
     })
 
     $('#projects-link').click(function() {
         console.log("Projects")
         $('#about-me-link').removeClass("active")
         $(this).addClass("active")
+
+        $('#home-page').addClass("hide")
+        $('#about-me-page').addClass("hide")
+        $('#projects-page').removeClass("hide")
     })
 
+    // HOME PAGE
     // Presentation
 
     var typed = new Typed(".auto-type", {

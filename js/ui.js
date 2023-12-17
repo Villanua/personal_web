726de4402c2cbbb107d98b22340265591ac6c21c
VillDev <NachoViCu16@gmail.com>
2023-12-17 20:42:38 +0100

    Home page

--- js/ui.js
+++ js/ui.js
@@ -4,24 +4,33 @@ $(document).ready(function() {
 
     console.log("Document loaded.")
     
+
     $('.logo').click(function() {
         console.log("Home")
         $('#projects-link').removeClass("active")
         $('#about-me-link').removeClass("active")
-        document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
     })
 
     $('#about-me-link').click(function() {
         console.log("About me")
         $('#projects-link').removeClass("active")
         $(this).addClass("active")
-        document.getElementById("content").innerHTML='<object type="text/html" data="about_me.html" ></object>';
     })
 
     $('#projects-link').click(function() {
         console.log("Projects")
         $('#about-me-link').removeClass("active")
         $(this).addClass("active")
-        document.getElementById("content").innerHTML='<object type="text/html" data="projects.html" ></object>';
     })
+
+    // Presentation
+
+    var typed = new Typed(".auto-type", {
+        strings: ["Robotics Engineer", "Passionate Programmer", "Dedicated Learner"],
+        typeSpeed: 150,
+        backSpeed: 50,
+        loop: true
+    })
+
+
 });

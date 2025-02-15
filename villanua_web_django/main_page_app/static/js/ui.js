b185c78c2b790fe0976997fa9cacd7925bd8369b
Villanua <NachoViCu16@gmail.com>
2025-02-15 23:51:22 +0100

    Add mobile & responsive app

--- villanua_web_django/main_page_app/static/js/ui.js
+++ villanua_web_django/main_page_app/static/js/ui.js
@@ -3,34 +3,31 @@ $(document).ready(function() {
 
     console.log("Document loaded.")
     
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        $('body').addClass('mobile');
+    }
+
     $('#logo-link').click(function() {
         console.log("Home")
-        $('#about-me-link').removeClass("active")
-        $('#projects-link').removeClass("active")
-
         $('#home-page').removeClass("hide")
         $('#about-me-page').addClass("hide")
         $('#projects-page').addClass("hide")
+        $(".navbar-collapse").collapse('hide');
     })
 
     $('#about-me-link').click(function() {
         console.log("About me")
-        $('#projects-link').removeClass("active")
-        $(this).addClass("active")
 
         $('#home-page').addClass("hide")
         $('#about-me-page').removeClass("hide")
         $('#projects-page').addClass("hide")
+        $(".navbar-collapse").collapse('hide');
     })
 
-    $('#projects-link').click(function() {
-        console.log("Projects")
-        $('#about-me-link').removeClass("active")
-        $(this).addClass("active")
-
-        $('#home-page').addClass("hide")
-        $('#about-me-page').addClass("hide")
-        $('#projects-page').removeClass("hide")
+    $('.project-link').click(function() {
+        console.log("Project link clicked")
+        $("#projectsAccordion").collapse('hide');
+        $(".navbar-collapse").collapse('hide');
     })
 
     // Detectar clics fuera del acordeón y del toggler y cerrarlos
@@ -52,7 +49,12 @@ $(document).ready(function() {
     // Presentation
 
     var typed = new Typed(".auto-type", {
-        strings: ["Robotics Engineer", "Software Programmer", "Constant Learner"],
+        strings: [
+            "Robotics and AI Engineer",
+            "Creative Mind", 
+            "Generative AI Developer", 
+            "Problem Solver",
+        ],
         typeSpeed: 150,
         backSpeed: 50,
         loop: true

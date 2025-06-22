de3b9475b2caf34ce7bd6766a4066c5b5b467feb
Villanua <NachoViCu16@gmail.com>
2025-06-22 21:57:22 +0200

    Add new structure of Ada

---
+++ villanua_web_django/villanua_web_project/shared/utils.py
@@ -0,0 +1,15 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configure logger to use the basic logging format
+logger = logging.getLogger(__name__)
+if not logger.hasHandlers():  # Only add handler if no handlers exist
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)s:%(filename)s:%(lineno)d:%(message)s')  # Format with source info
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+if eval(os.environ.get("INGESTION_DEBUG", "False")):
+    logger.setLevel(logging.DEBUG)
